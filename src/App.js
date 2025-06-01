002cb7c8391abe080ec62c3478b235f290d19811
bestdevelope <leabouillot7@gmail.com>
2025-06-01 18:31:05 +0200

    Mutation

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ const client = new ApolloClient({
 });
 
 function App() {
-  const [menu, setMenu] = useState("Roles");
+  //const [menu, setMenu] = useState("Roles");
+  const [menu, setMenu] = useState("Teams");
 
   let mainComp = {
     Roles: <Roles />,
